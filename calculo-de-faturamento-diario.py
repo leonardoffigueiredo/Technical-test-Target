dfed4f3946d4bceb4f58f4fe2eed26560ef64c43
Leonardo F. Figueiredo <145238843+leonardoffigueiredo@users.noreply.github.com>
2024-09-04 10:48:27 -0300

    Update calculo-de-faturamento-diario.py

--- calculo-de-faturamento-diario.py
+++ calculo-de-faturamento-diario.py
@@ -1,24 +1,140 @@
 import json
+from statistics import mean
 
 dados_json = '''
 [
-  {"dia": 1, "valor": 1000.0},
-  {"dia": 2, "valor": 0.0},
-  {"dia": 3, "valor": 1500.0},
-  {"dia": 4, "valor": 0.0},
-  {"dia": 5, "valor": 2000.0}
+	{
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
 ]
 '''
 
 dados = json.loads(dados_json)
 
-valores = [d["valor"] for d in dados if d["valor"] > 0]  
+valores = [d["valor"] for d in dados if d["valor"] > 0]
 
 menor_valor = min(valores)
 maior_valor = max(valores)
-media_mensal = sum(valores) / len(valores)
+media_mensal = mean(valores)
 dias_acima_da_media = sum(1 for v in valores if v > media_mensal)
 
-print(f"Menor valor de faturamento: {menor_valor}")
-print(f"Maior valor de faturamento: {maior_valor}")
-print(f"Dias com faturamento acima da média: {dias_acima_da_media}")
+print(f"Menor valor de faturamento: R$ {menor_valor:.2f}")
+print(f"Maior valor de faturamento: R$ {maior_valor:.2f}")
+print(f"Número de dias com faturamento acima da média mensal: {dias_acima_da_media}")
